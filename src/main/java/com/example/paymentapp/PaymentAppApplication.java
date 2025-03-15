f09d87e60fee0c6500abdd31b6082407e2dbb950
Madi Zhaksykeldi <madizhaksykeldi@Madis-Air-4.Dlink>
2025-03-15 16:18:08 +0500

    feat: Add exception handling

--- src/main/java/com/example/paymentapp/PaymentAppApplication.java
+++ src/main/java/com/example/paymentapp/PaymentAppApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PaymentAppApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PaymentAppApplication.class, args);
     }
-
 }
